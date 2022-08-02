328ab6070ca32b6c01f8cae70c42e3de804cd556
Dominick Muniz <57696711+DominickMuniz@users.noreply.github.com>
2022-08-01 23:36:54 -0600

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,15 @@
+
+def is_palindrome(input_string):
+    original_string = ""
+    reverse_string = ""
+    
+    for letter in input_string.strip():
+	  # Add non-blank letters to the 
+		# end of one string, and to the front
+		# of the other string
+        new_string = new_string+letter.replace(" ","")
+        reverse_string = letter.replace(" ", "")+ reverse_string
+	
+    if new_string.lower() == reverse_string.lower():
+	    return True
+    return False 
